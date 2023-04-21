039c3a01b1160517a9512eee7c75430b8ba96931
Mahdi0024 <0024mahdi@gmail.com>
2023-04-21 19:46:45 +0330

    Improved testing speed.

---
+++ ZitelAutoFreqTool/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.NetworkInformation;
+
+namespace ZitelAutoFreqTool;
+
+internal static class Helpers
+{
+    public static async Task WaitForInternetConnection()
+    {
+        using var p = new Ping();
+        while (true)
+        {
+            try
+            {
+                var pingReply = await p.SendPingAsync("4.2.2.4");
+                if (pingReply.Status is IPStatus.Success)
+                    return;
+            }
+            catch
+            {
+                continue;
+            }
+        }
+    }
+}
